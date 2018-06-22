53722baf065fe05ddfceb2d9286286be0ba82c5c
小野 裕生 <y.ono@mac0061.local>
2018-06-22 16:27:58 +0900

    change file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,36 @@
 require_relative '../validator/common_validator'
 require_relative './concerns/common_checker'
 
+
 include Common_Checker
 include Common_Validator
 
+
 class PostsController < ApplicationController
 
-     def result
 
-        # 値を受け取る
-        @post = params[:content]
-        # binding.pry
-        # Validation
-        hand_valid
+  def result
 
-        # 役判定
-        if @common_error_array.last == "no problem"
-          hand_check
-        else
-          @error_message = @common_error_array[0]
-        end
 
-        # topページに戻る
-        render("home/top")
+    # 値を受け取る
+    @post = params[:content]
 
-     end
+    # Validation
+    hand_valid
+
+    # 役判定
+    if @common_error_array.last == "no problem"
+      hand_check
+    else
+      @error_message = @common_error_array[0]
+    end
+
+
+    # topページに戻る
+    render("home/top")
+
+
+  end
+
 
 end
-
-
